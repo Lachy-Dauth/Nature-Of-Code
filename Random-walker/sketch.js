eb174095d29f644b2ab7a4ccb4ede787712176a1
Lachlan Dauth <174690@SCT-lachlan.dauth-174690>
2022-11-30 11:37:22 +0800

    finished random walker program

---
+++ Random-walker/sketch.js
@@ -0,0 +1,31 @@
+let x;
+let y;
+
+function setup() {
+  createCanvas(2000, 2000);
+  x = 1000;
+  y = 1000;
+  background(51);
+}
+
+function draw() {
+  stroke(255)
+  strokeWeight(2)
+  for (let i = 0; i < 1000; i++) {
+    point(x, y);
+    let randint = floor(random(4))
+    switch(randint){
+      case 0:
+        x ++;
+        break;
+      case 1:
+        x --;
+        break;
+      case 2:
+        y ++;
+        break;
+      case 3:
+        y --;
+    }
+  }
+}
