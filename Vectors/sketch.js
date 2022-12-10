0f70d9833572889599712a7fed211bf85d2fa4e4
Lachlan Dauth <174690@SCT-lachlan.dauth-174690>
2022-12-10 08:09:09 +0800

    finished vectors 1.1 OOP and walkers

--- Vectors/sketch.js
+++ Vectors/sketch.js
@@ -1,7 +1,14 @@
+let walker;
+
 function setup() {
   createCanvas(400, 400);
+  walker = new Walker(width/2, height/2);
+  background(51);
 }
 
 function draw() {
-  background(220);
+  stroke(255)
+  strokeWeight(2)
+  point(walker.pos);
+  walker.update();
 }
